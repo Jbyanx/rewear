dc9db13f741517841e55536d58cfc72d3c015506
Jbyanx <108432086+Jbyanx@users.noreply.github.com>
2025-09-26 03:34:09 -0500

    feat(controller): creando user controller vacío

---
+++ src/main/java/com/devops/backend/rewear/controllers/UserController.java
@@ -0,0 +1,17 @@
+package com.devops.backend.rewear.controllers;
+
+import com.devops.backend.rewear.services.UserService;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/users")
+public class UserController {
+    private final UserService userService;
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+
+}
