b83f60cbaff970743fd7aec509de926b5fde088a
Jbyanx <108432086+Jbyanx@users.noreply.github.com>
2025-09-26 02:11:36 -0500

    feat(handler): capturando excepciones para cuando exista el username y cuando exista el email

---
+++ src/main/java/com/devops/backend/rewear/exceptions/GlobalExceptionHandler.java
@@ -0,0 +1,26 @@
+package com.devops.backend.rewear.exceptions;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(UsernameAlreadyExistsException.class)
+    public ResponseEntity<Map<String, String>> handleUsernameAlreadyExistsException(UsernameAlreadyExistsException ex) {
+        Map<String, String> response = new HashMap<>();
+        response.put("error", "El nombre de usuario ya está en uso");
+        return ResponseEntity.badRequest().body(response);
+    }
+    @ExceptionHandler(EmailAlreadyExistsException.class)
+    public ResponseEntity<Map<String, String>> handleEmailAlreadyExistsException(EmailAlreadyExistsException ex) {
+        Map<String, String> response = new HashMap<>();
+        response.put("error", "El email ya está en uso");
+        return ResponseEntity.badRequest().body(response);
+    }
+}
+
