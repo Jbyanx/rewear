27fa1ab8201e07eb2d59b199ba54915c50509f8f
Jbyanx <108432086+Jbyanx@users.noreply.github.com>
2025-09-26 17:26:30 -0500

    feat(controller): creando controlador de autenticacion (/auth) con endpoints: - /login - /register

--- src/main/java/com/devops/backend/rewear/controllers/AuthController.java
+++ src/main/java/com/devops/backend/rewear/controllers/AuthController.java
@@ -1,8 +1,11 @@
 package com.devops.backend.rewear.controllers;
 
+import com.devops.backend.rewear.dtos.request.LoginRequest;
 import com.devops.backend.rewear.dtos.request.SaveUser;
 import com.devops.backend.rewear.dtos.response.GetUserProfile;
+import com.devops.backend.rewear.dtos.response.LoginResponse;
 import com.devops.backend.rewear.services.UserService;
+import com.devops.backend.rewear.services.impl.AuthService;
 import jakarta.validation.Valid;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -13,14 +16,19 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/auth")
 public class AuthController {
-    private final UserService userService;
+    private final AuthService authService;
 
-    public AuthController(UserService userService) {
-        this.userService = userService;
+    public AuthController(AuthService authService) {
+        this.authService = authService;
     }
 
     @PostMapping("/register")
     public ResponseEntity<GetUserProfile> register(@Valid @RequestBody SaveUser saveUser) {
-        return ResponseEntity.ok(userService.save(saveUser));
+        return ResponseEntity.ok(authService.register(saveUser));
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<LoginResponse> login(@Valid @RequestBody LoginRequest loginRequest) {
+        return ResponseEntity.ok(authService.login(loginRequest));
     }
 }
