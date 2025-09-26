3246d29089a7b2d81946e2bcbf393ef8dd86ad0f
Jbyanx <108432086+Jbyanx@users.noreply.github.com>
2025-09-26 00:28:10 -0500

    chore: commit inicial con la configuracion del proyecto

---
+++ src/test/java/com/devops/backend/rewear/ReWearApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devops.backend.rewear;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReWearApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
