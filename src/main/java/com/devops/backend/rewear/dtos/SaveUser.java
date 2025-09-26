194aad8af8173d877f0a0267913e32fa2d2ada09
Jbyanx <108432086+Jbyanx@users.noreply.github.com>
2025-09-26 00:31:56 -0500

    feat(dto): creando dto para guardar usuarios

---
+++ src/main/java/com/devops/backend/rewear/dtos/SaveUser.java
@@ -0,0 +1,25 @@
+package com.devops.backend.rewear.dtos;
+
+import com.devops.backend.rewear.entities.enums.DocumentType;
+import com.devops.backend.rewear.entities.enums.Genre;
+
+import java.io.Serializable;
+import java.time.LocalDate;
+
+public record SaveUser(
+        String firstName,
+        String lastName,
+        String phoneNumber,
+        String email,
+        String username,
+        String password,
+        String address,
+        String city,
+        String country,
+        LocalDate birthDate,
+        Genre genre,
+        DocumentType documentType,
+        String documentNumber,
+        String profileImageUrl
+) implements Serializable {
+}
