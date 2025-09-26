97fb7096fbccdf3d4f5e703932fed236c2ed2976
Jbyanx <108432086+Jbyanx@users.noreply.github.com>
2025-09-26 02:10:12 -0500

    feat(exception): creando excepcion personalizada para cuando el email ya exista

---
+++ src/main/java/com/devops/backend/rewear/exceptions/EmailAlreadyExistsException.java
@@ -0,0 +1,7 @@
+package com.devops.backend.rewear.exceptions;
+
+public class EmailAlreadyExistsException extends RuntimeException {
+    public EmailAlreadyExistsException(String message) {
+        super(message);
+    }
+}
