47900f38209d96ebab10b43f6cb58263020db541
Jbyanx <108432086+Jbyanx@users.noreply.github.com>
2025-09-26 02:45:27 -0500

    feat(dto): creando un dto pra el perfil detallado del usuario

---
+++ src/main/java/com/devops/backend/rewear/dtos/response/GetMyUserProfile.java
@@ -0,0 +1,30 @@
+package com.devops.backend.rewear.dtos.response;
+
+import com.devops.backend.rewear.entities.enums.DocumentType;
+import com.devops.backend.rewear.entities.enums.Genre;
+
+import java.io.Serializable;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+public record GetMyUserProfile(
+        Long id,
+        String firstName,
+        String lastName,
+        String phoneNumber,
+        String email,
+        String username,
+        String address,
+        String city,
+        String country,
+        LocalDate birthDate,
+        Genre genre,
+        DocumentType documentType,
+        String documentNumber,
+        String profileImageUrl,
+        Double rating,
+        Integer totalRatings,
+        LocalDateTime createdAt,
+        String role
+) implements Serializable {
+}
