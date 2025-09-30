9b7a440407479bbeec7a155f71762cd492870171
Jbyanx <108432086+Jbyanx@users.noreply.github.com>
2025-09-30 18:22:17 -0500

    feat(users): creando endpoint para traer un usuario por su id /api/users/{id}

--- src/main/java/com/devops/backend/rewear/controllers/UserController.java
+++ src/main/java/com/devops/backend/rewear/controllers/UserController.java
@@ -6,6 +6,7 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -22,6 +23,11 @@ public class UserController {
     public ResponseEntity<Page<GetUser>> getAllUsers(Pageable pageable) {
         Page<GetUser> getUserPage = userService.getUsers(pageable);
         return ResponseEntity.ok(getUserPage);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<GetUser> getUser(@PathVariable Long id) {
+        return ResponseEntity.ok(userService.getById(id));
     }
 
 }
