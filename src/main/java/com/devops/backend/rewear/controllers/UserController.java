9160d2f6c3069e0fa1823ef0ee07f8d46eac1dc5
Jbyanx <108432086+Jbyanx@users.noreply.github.com>
2025-09-30 18:18:17 -0500

    feat(users): creando endpoint para traer una pagina de usuarios, espera estos parámetros en la URL:     page → número de página (empieza en 0)     size → cuántos registros por página     sort → campo(s) por los que ordenar, ejemplo sort=nombre,asc por defecto: page = 0 → siempre arranca en la primera página. size = 20 → 20 elementos por página. sort = UNPAGED → sin ordenamiento (queda como venga de la BD).

--- src/main/java/com/devops/backend/rewear/controllers/UserController.java
+++ src/main/java/com/devops/backend/rewear/controllers/UserController.java
@@ -1,6 +1,11 @@
 package com.devops.backend.rewear.controllers;
 
+import com.devops.backend.rewear.dtos.response.GetUser;
 import com.devops.backend.rewear.services.UserService;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -13,5 +18,10 @@ public class UserController {
         this.userService = userService;
     }
 
+    @GetMapping
+    public ResponseEntity<Page<GetUser>> getAllUsers(Pageable pageable) {
+        Page<GetUser> getUserPage = userService.getUsers(pageable);
+        return ResponseEntity.ok(getUserPage);
+    }
 
 }
