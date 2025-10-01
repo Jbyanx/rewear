f0222180c94d14263014f40af2d4927559c6e78f
Jbyanx <108432086+Jbyanx@users.noreply.github.com>
2025-09-30 22:50:42 -0500

    feat: creando query method para obtener entidad User por su username

--- src/main/java/com/devops/backend/rewear/repositories/UserRepository.java
+++ src/main/java/com/devops/backend/rewear/repositories/UserRepository.java
@@ -8,6 +8,7 @@ import java.util.Optional;
 public interface UserRepository extends JpaRepository<User, Long> {
     Optional<User> findByUsername(String username);
     Optional<User> getByUsername(String username);
+    Optional<User> getReferenceByUsername(String username);
 
     boolean existsByEmail(String email);
 
