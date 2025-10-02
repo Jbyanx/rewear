9fe20e8512a65d36810a4fc626a8761abe8dcab8
Jbyanx <108432086+Jbyanx@users.noreply.github.com>
2025-10-02 11:57:16 -0500

    secure: aseguramos el metodo getAllUsers

--- src/main/java/com/devops/backend/rewear/controllers/UserController.java
+++ src/main/java/com/devops/backend/rewear/controllers/UserController.java
@@ -9,6 +9,7 @@ import jakarta.validation.Valid;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -26,6 +27,7 @@ public class UserController {
         return ResponseEntity.ok(getUserPage);
     }
 
+    @PreAuthorize("hasRole('ADMIN')")
     @GetMapping("/all")
     public ResponseEntity<Page<GetUser>> getAllUsers(Pageable pageable) {
         Page<GetUser> getUserPage = userService.getAllUsers(pageable);
